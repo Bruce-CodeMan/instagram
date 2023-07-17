08a90c5fb0bf227438428a7fba2ee8650ef82031
Bruce <1033684650@qq.com>
2023-07-17 23:29:48 +0800

    finish the post page

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -1,5 +1,5 @@
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
-import { HeartIcon, ChatBubbleOvalLeftEllipsisIcon, BookmarkIcon } from "@heroicons/react/24/outline";
+import { HeartIcon, ChatBubbleOvalLeftEllipsisIcon, BookmarkIcon, FaceSmileIcon } from "@heroicons/react/24/outline";
 
 
 
@@ -31,6 +31,19 @@ const Post = ({ username, userImg, img, caption }: IProps) => {
                 </div>
                 <BookmarkIcon className="h-7 hover:scale-125 transition-transform duration-200 ease-out"/>
             </div>
+
+            {/* Post comments */}
+            <p className="p-5 truncate">
+                <span className="font-bold mr-5">{ username }</span>
+                <span>{ caption }</span>
+            </p>
+
+            {/* Post Input box */}
+            <form action="" className="flex items-center p-4 space-x-4">
+                <FaceSmileIcon className="h-7"/>
+                <input type="text" placeholder="Enter your comments" className="border-none flex-1"/>
+                <button className="text-blue-500 font-bold">Post</button>
+            </form>
         </div>
     )
 }
