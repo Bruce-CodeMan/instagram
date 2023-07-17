4300e31a7a7cbe0ca78abdfe0897ac1aaf2117a0
Bruce <1033684650@qq.com>
2023-07-17 23:46:33 +0800

    finish the miniprofile component

--- src/containers/Content/index.tsx
+++ src/containers/Content/index.tsx
@@ -1,20 +1,25 @@
 import Stories from "../Stories";
 import Posts from "../Posts";
+import MiniPorfile from "../MiniProfile";
 
 const Content = () => {
     return (
-        <div>
-            <section>
+        <div className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+            <section className="md:col-span-2">
                 {/* Stories */}
                 <Stories />
                 {/* Posts */}
                 <Posts />
             </section>
 
-            <section>
-                {/* Mini Profile */}
+            <section className="hidden md:inline-grid md:col-span-1">
+                <div className="fixed w-[380px]">
+                    {/* Mini Profile */}
+                    <MiniPorfile />
+                    {/* Suggections */}
 
-                {/* Suggections */}
+                </div>
+
             </section>
         </div>
     )
---
+++ src/containers/MiniProfile/index.tsx
@@ -0,0 +1,14 @@
+const MiniPorfile = () => {
+    return (
+        <div className="flex items-center justify-between mt-14 ml-10">
+            <img src="https://i.pravatar.cc/150?img=1" alt="" className="h-16 border p-[2px] rounded-full cursor-pointer"/>
+            <div className="flex-1 ml-4">
+                <h2 className="font-bold">xxx</h2>
+                <h3 className="text-sm text-gray-400">yyyy</h3>
+            </div>
+            <button className="font-semibold text-blue-400 text-sm">Signout</button>
+        </div>
+    )
+}
+
+export default MiniPorfile;
